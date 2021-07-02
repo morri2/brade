c4fee0754329f2a26c53afa2948a84a83176b098
Morris Hansing <morris@hansing.se>
2021-07-02 14:05:36 +0200

    make board data set fns

--- src/game/board.rs
+++ src/game/board.rs
@@ -4,8 +4,10 @@ use super::position::*;
 const POINT_STEP: usize = 5; // bits to step to get next points in marker counter
 const BOARD_MASK: u128 = (1 << 120)-1;
 
+const POINT_DATA_MASK: u128 = 0b11111;
 const MARKER_COUNT_MASK: u8 = 0b01111; 
 const MARKER_COLOR_MASK: u8 = 0b10000;
+
 
 const CLOSABLE: u128 = 0b0;
 
@@ -77,6 +79,34 @@ impl Board {
         self.get_color(pos) == Some(color)
     }
 
+
+
+    fn set_point_data(&mut self, pos: Position, data: u8) {
+        if let Position::Point(point) = pos {
+            let data = self.get_point_data(pos);
+            self.board_data &= !POINT_DATA_MASK << (POINT_STEP * point);
+            self.board_data |= (data as u128) << (POINT_STEP * point);
+        }
+    }
+
+    fn set_marker_count(&mut self, pos: Position, count: u8) {
+        let color_data = self.get_point_data(pos) & MARKER_COLOR_MASK;
+        let count_data = count & MARKER_COUNT_MASK;
+        self.set_point_data(pos, color_data | count_data);
+    }
+
+    fn set_marker_color(&mut self, pos:Position, color: Color) {
+        let color_data = (color.index() as u8) << 4;
+        let count_data = self.get_point_data(pos) & MARKER_COLOR_MASK;
+        self.set_point_data(pos, color_data | count_data);
+    }
+
+    fn set_marker_color_count(&mut self, pos: Position, color: Color, count: u8) {
+        let color_data = (color.index() as u8) << 4;
+        let count_data = count & MARKER_COLOR_MASK;
+        self.set_point_data(pos, color_data | count_data);
+    }
+
     fn is_legal_dest(&self, pos: Position, color: Color) -> bool {
         if let Some(pcolor) = self.get_color(pos) {
             if pcolor == color {
@@ -90,6 +120,6 @@ impl Board {
     }
 
     fn apply_submove(submove: Submove) {
-
+        
     }
 }
