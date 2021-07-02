981adcebc08a6be047a2a573ee6dcc7a9f2e1647
Morris Hansing <morris@hansing.se>
2021-07-02 11:29:46 +0200

    make board take pos

--- src/game/board.rs
+++ src/game/board.rs
@@ -1,10 +1,11 @@
 use super::color::*;
 use super::r#move::*;
+use super::position::*;
 const POINT_STEP: usize = 5; // bits to step to get next points in marker counter
 const BOARD_MASK: u128 = (1 << 120)-1;
 
-const MARKER_COUNT_MASK: u128 = 0b1111; 
-const MARKER_COLOR_MASK: u128 = 0b10000;
+const MARKER_COUNT_MASK: u8 = 0b01111; 
+const MARKER_COLOR_MASK: u8 = 0b10000;
 
 const CLOSABLE: u128 = 0b0;
 
@@ -27,30 +28,42 @@ impl Board {
         }
     }
 
+    fn is_closable(pos: Position) -> bool{
+        match pos {
+            Position::Point(0..=10) => false,
+            _ => true, // bar and out are closable just in case
+        } 
+    }
+
     fn flip(&mut self){
        self.board_data = ((self.board_data >> 12 * POINT_STEP) | (self.board_data << 12 * POINT_STEP)) & BOARD_MASK
     }
 
-    fn get_point_data(&self, point: usize) -> u128{
-        assert!(point < 24);
-        self.board_data >> (point * POINT_STEP)
+    fn get_point_data(&self, pos: Position) -> u8{
+        match pos {
+            Position::Point(point) => {(self.board_data >> (point * POINT_STEP)) as u8},
+            _ => 0 // maybe option?
+        }
     }
 
-    fn get_marker_count(&self, point: usize) -> u8{
-        (self.get_point_data(point) * MARKER_COUNT_MASK) as u8
+    fn get_marker_count(&self, pos: Position) -> u8{
+        match pos {
+            Position::Point(_) => {(self.get_point_data(pos) * MARKER_COUNT_MASK) as u8},
+            _ => 0
+        }
     }
 
-    fn is_singleton(&self, point: usize) -> bool {
-        self.get_marker_count(point) == 1
+    fn is_singleton(&self, pos: Position) -> bool {
+        self.get_marker_count(pos) == 0
     }
 
-    fn is_occupied(&self, point: usize) -> bool {
-        self.get_marker_count(point) != 0
+    fn is_occupied(&self, pos: Position) -> bool {
+        self.get_marker_count(pos) != 0
     }
 
-    fn get_color(&self, point: usize) -> Option<Color>{
-        if self.is_occupied(point) {
-            if self.get_point_data(point) & MARKER_COLOR_MASK == 0 /*White is index 0*/{ 
+    fn get_color(&self, pos: Position) -> Option<Color>{
+        if self.is_occupied(pos) {
+            if self.get_point_data(pos) & MARKER_COLOR_MASK == 0 /*White is index 0*/{ 
                 Some(Color::White)
             } else {
                 Some(Color::Black)
@@ -60,16 +73,16 @@ impl Board {
         }
     }
 
-    fn is_color(&self, point: usize, color: Color) -> bool {
-        self.get_color(point) == Some(color)
+    fn is_color(&self, pos: Position, color: Color) -> bool {
+        self.get_color(pos) == Some(color)
     }
 
-    fn is_legal_dest(&self, point: usize, color: Color) -> bool {
-        if let Some(pcolor) = self.get_color(point) {
+    fn is_legal_dest(&self, pos: Position, color: Color) -> bool {
+        if let Some(pcolor) = self.get_color(pos) {
             if pcolor == color {
                 return /*closeable*/ false  
             } else {
-                return self.is_singleton(point) || /*sprängable*/ false
+                return self.is_singleton(pos) || /*sprängable*/ false
             }
         } else {
             true
@@ -78,6 +91,5 @@ impl Board {
 
     fn apply_submove(submove: Submove) {
 
-        
     }
 }
